50d481fc040b2fe55e45d2f3ced0f8fd66d4428f
Sandro-Feijo <sandrofeijo.aluno@unipampa.edu.br>
2025-06-06 15:37:07 -0300

    update de tratamento após testes de funcionalidades
    parâmetros levam em conta testar.sh, limitando suas alterações para não gerar a necessidade de afetar múltiplos scripts

--- tratamento.sh
+++ tratamento.sh
@@ -3,17 +3,18 @@
 
 
 
-#
+#verificacao de parametro ausente
 
-while getopts ":a:l:g:" opt; do
+while getopts ":a:l:g:t:" opt; do
     case "${opt}" in
         a) entrada=${OPTARG};;
         l) linguagem=${OPTARG};;
         g) algoritmo=${OPTARG};;
+        t) tamanho=${OPTARG};;
         *)
 	
 		echo "Erro!!"
-		echo "esperado o Uso de: $0 -a <arquivo.csv> -l <linguagem> -g <algoritmo>"; exit 1
+		echo "esperado o Uso de: $0 -a <arquivo.csv> -l <linguagem> -g <algoritmo> -t <tamanho>"; exit 1
 		;;
     	esac
 done
@@ -23,11 +24,11 @@ done
 
 
 
-#
+#calcula a media da primeira coluna, caso vazio retorna 0
 
-if [[ -z "$entrada" || -z "$linguagem" || -z "$algoritmo" ]]; then
+if [[ -z "$entrada" || -z "$linguagem" || -z "$algoritmo" || -z "$tamanho" ]]; then
     echo "Erro: argumentos obrigatorios não forneicidos ou incompletos"
-    echo "esperado o Uso de: $0 -a <arquivo.csv> -l <linguagem> -g <algoritmo>"
+    echo "esperado o Uso de: $0 -a <arquivo.csv> -l <linguagem> -g <algoritmo> -t <tamanho>"
     exit 1
 fi
 
@@ -44,12 +45,12 @@ media=$(awk '{s+=$1} END {if (NR > 0) print s/NR; else print 0}' "$entrada")
 
 
 
-#
+#definicao do caminho aonde a saida sera salva
 saidas="saidas/medias_consolidadas.csv"
 
 
 
-#
+#verifica se ja existe uma saida( gerada de uma interacao anterior)
 if [[ ! -f "$saidas" ]]; then
     echo "linguagem;algoritmo;media" > "$saidas"
 fi
@@ -57,7 +58,8 @@ fi
 
 
 
-#
+#adiciona uma linha de saida com os dados da media processada nessa interacao
+
 echo "$linguagem;$algoritmo;$media" >> "$saidas"
 
 echo "Finalizado! resultados atualizados em: $saidas"
