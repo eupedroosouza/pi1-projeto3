16bf130c27fad686eb0d292774fe282c78ef137c
Sandro-Feijo <sandrofeijo.aluno@unipampa.edu.br>
2025-06-07 00:19:50 -0300

    Update do tratamento.sh - agora cada media é separada em linguagem e tipo de ordenação
    Update do tratamento.sh - agora cada media é separada em linguagem e tipo de ordenação totalizando 4 , aos quais estão em extensão .cvs

--- tratamento.sh
+++ tratamento.sh
@@ -39,20 +39,20 @@ fi
 
 
 #
-media=$(awk '{s+=$1} END {if (NR > 0) print s/NR; else print 0}' "$entrada")
+media=$(awk '{s+=$1} END {if (NR > 0) printf "%.8f\n", s/NR; else print 0}' "$entrada")
 
 
 
 
 
 #definicao do caminho aonde a saida sera salva
-saidas="saidas/medias_consolidadas.csv"
+saidas="saidas/${linguagem}_${algoritmo}.csv"
 
 
 
 #verifica se ja existe uma saida( gerada de uma interacao anterior)
 if [[ ! -f "$saidas" ]]; then
-    echo "linguagem;algoritmo;media" > "$saidas"
+    echo "linguagem;algoritmo;tamanho;media" > "$saidas"
 fi
 
 
@@ -60,7 +60,7 @@ fi
 
 #adiciona uma linha de saida com os dados da media processada nessa interacao
 
-echo "$linguagem;$algoritmo;$media" >> "$saidas"
+echo "$linguagem;$algoritmo;10^$tamanho;$media" >> "$saidas"
 
 echo "Finalizado! resultados atualizados em: $saidas"
 
